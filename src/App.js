74fd357a0d670b76c9d628b672c2604c12e79a9c
arko489 <50736196+arko489@users.noreply.github.com>
2019-07-12 10:53:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello 
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=flt-8OxfIQQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kill Thanos
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
